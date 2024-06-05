dcb11699828a060a04b792f50e878f35845f8bc7
Fadhil <fadhilahdwi.a@gmail.com>
2024-06-05 11:19:55 +0700

    feat: fix home section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
-import { HomeSection } from '@components/Section/HomeSection';
+import { HomeSection } from '@components/HomeSection';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className="container mx-auto w-full min-h-screen flex-col justify-center items-center mt-20">
+      <Image src="/images/hero-bg.png" alt="star" fill />
       <HomeSection />
     </div>
   );
