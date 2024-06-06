65fb19148df6365668600544a0cfadd3ed734e50
Fadhil <fadhilahdwi.a@gmail.com>
2024-06-06 12:26:58 +0700

    update

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,23 +1,57 @@
 import React from 'react';
 
+const skillItems = [
+  'React',
+  'React Native',
+  'Next.js',
+  'JavaScript',
+  'TypeScript',
+  'HTML',
+  'CSS',
+  'Sass',
+  'Tailwind',
+  'Material-UI',
+  'Git',
+  'Framer Motion',
+  'Styled-Components',
+  'Vue.js',
+  'Svelte.js',
+  'Express.js',
+  'Firebase',
+  'Jest',
+  'Scrum',
+  'Node.js',
+  'Nuxt.js',
+  'Postman'
+];
+
 export const AboutSection = () => {
   return (
     <div id="about" className="flex flex-col gap-40">
       <div className="flex flex-col gap-4">
         <h1 className="text-2xl font-semibold">About</h1>
         <p className="font-light text-zinc-400">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius molestiae aperiam quas
-          perspiciatis beatae harum animi assumenda reiciendis dolor temporibus delectus voluptas
-          exercitationem dignissimos, nam placeat voluptatem minima. Ipsum, in.
+          I am an experienced front-end developer with 3+ years of experience. I specialize in
+          designing and developing visually appealing and responsive user interfaces. Throughout my
+          career, I have successfully collaborated with teams to deliver innovative and user-focused
+          solutions. I have a strong ability to learn quickly and I am always seeking opportunities
+          to deepen my knowledge and improve my IT skills. I am ready to leverage my technical
+          expertise to benefit others and organizations
         </p>
       </div>
       <div className="flex flex-col gap-4">
         <h1 className="text-2xl font-semibold">Skills</h1>
-        <p className="font-light text-zinc-400">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius molestiae aperiam quas
-          perspiciatis beatae harum animi assumenda reiciendis dolor temporibus delectus voluptas
-          exercitationem dignissimos, nam placeat voluptatem minima. Ipsum, in.
-        </p>
+        <div className="flex font-light text-sm text-[#bbb] gap-2 flex-wrap">
+          {skillItems.map((item, index) => (
+            <span key={index} className="p-[6px] bg-[#232323] border border-1 border-[#333] shadow">
+              {item}
+            </span>
+          ))}
+        </div>
+      </div>
+      <div className="flex flex-col gap-4">
+        <h1 className="text-2xl font-semibold">Experience</h1>
+        <div className="flex font-light text-sm text-[#bbb] gap-2 flex-wrap"></div>
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import { AboutSection } from '@components/AboutSection';
 import { HomeSection } from '@components/HomeSection';
+import Head from 'next/head';
 import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="container mx-auto w-full min-h-screen flex-col justify-center items-center">
+    <div className="mx-5 sm:container sm:mx-auto w-full min-h-screen flex-col justify-center items-center">
+      <Head>
+        <title>Fadhilah Dwi Ananda</title>
+      </Head>
       <Image src="/images/hero-bg.png" alt="star" fill />
       <Image src="/images/gradient.webp" alt="star" fill />
       <HomeSection />
